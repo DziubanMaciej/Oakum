4c5952c998b4abcb6493c79fb6909e2f04507ebe
MaciejDziuban <dziuban.maciej@gmail.com>
2021-08-15 16:15:00 +0200

    Fix error.h macros

--- source/error.h
+++ source/error.h
@@ -7,7 +7,9 @@ void performAbort() {
     throw std::exception{};
 }
 
-inline void dumpLog(std::ostream &out) {}
+inline void dumpLog(std::ostream &out) {
+    out << std::endl;
+}
 
 template <typename Arg, typename... Args>
 inline void dumpLog(std::ostream &out, Arg &&arg, Args &&...args) {
@@ -16,19 +18,19 @@ inline void dumpLog(std::ostream &out, Arg &&arg, Args &&...args) {
 }
 } // namespace Oakum
 
-#define FATAL_ERROR(message, ...)                          \
-    Oakum::dumpLog(std::cerr, message, __VA_ARGS__, '\n'); \
+#define FATAL_ERROR(...)                          \
+    Oakum::dumpLog(std::cerr, __VA_ARGS__); \
     Oakum::performAbort();
 
-#define FATAL_ERROR_IF(condition, message, ...) \
+#define FATAL_ERROR_IF(condition, ...) \
     if (condition) {                            \
-        FATAL_ERROR(message, __VA_ARGS__);      \
+        FATAL_ERROR(__VA_ARGS__);      \
     }
 
 #ifdef _DEBUG
-#define DEBUG_ERROR(message, ...) FATAL_ERROR(message, ...)
-#define DEBUG_ERROR_IF(condiation, message, ...) FATAL_ERROR_IF(condition, message, ...)
+#define DEBUG_ERROR(...) FATAL_ERROR( __VA_ARGS__)
+#define DEBUG_ERROR_IF(condition, ...) FATAL_ERROR_IF(condition,  __VA_ARGS__)
 #else
-#define DEBUG_ERROR(message, ...)
-#define DEBUG_ERROR_IF(condiation, message, ...)
+#define DEBUG_ERROR(...)
+#define DEBUG_ERROR_IF(condition, ...)
 #endif
